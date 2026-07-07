90855f5d960eea608ef2a7cdb887d4cc4b584ab7
sahildhatwalia <sahildhatwalia10@gmail.com>
2026-07-07 11:28:04 +0530

    update

--- controller/controller.js
+++ controller/controller.js
@@ -66,7 +66,7 @@ const transporter=nodemailer.createTransport({
     secure:true,
     auth:{
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS
     }
 
 })
