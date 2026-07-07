d184cbaa7d66912d9fd910e31a2d59f78bd26d0a
sahildhatwalia <sahildhatwalia10@gmail.com>
2026-07-07 11:08:22 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
-
+require("dotenv").config();
 const PORT=4000
 
 const app=express()
--- controller/controller.js
+++ controller/controller.js
@@ -65,8 +65,8 @@ const transporter=nodemailer.createTransport({
     port:465,
     secure:true,
     auth:{
-        user:"sahildhatwalia04@gmail.com",
-        pass:"blxy uzex aczo jkdh"
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     }
 
 })
