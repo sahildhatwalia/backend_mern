fb520de700404bacb44863808b142b66aa737759
sahildhatwalia <sahildhatwalia10@gmail.com>
2026-07-17 12:06:44 +0530

    finallly tarining ends huhhhhhhhhhhh

--- app.js
+++ app.js
@@ -24,6 +24,19 @@ mongoose.connect("mongodb://localhost:27017/node")
     console.error("Error connecting to MongoDB:", err)
 })
 app.use("/uploads", express.static("uploads"));
+
+app.get("/api/firebase-config", (req, res) => {
+    res.json({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    });
+});
+
 app.use(express.static("."));
 app.use(express.json())
 app.use("/api",router)
